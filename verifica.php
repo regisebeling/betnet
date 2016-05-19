55cdfa78149b8abd2aebb0d62b1bb8b377134c84
Régis Ebeling <regisebeling@gmail.com>
2016-05-19 02:33:27 -0300

    Verifica se existe uma sessão ativa

---
+++ verifica.php
@@ -0,0 +1,23 @@
+<?php
+
+// Inicia sessões
+
+session_start();
+
+
+
+// Verifica se existe os dados da sessão de login
+
+if(!isset($_SESSION["id_usuario"]) || !isset($_SESSION["nome_usuario"]))
+
+{
+
+    // Usuário não logado! Redireciona para a página de login
+
+    header("Location: index.php");
+
+    exit;
+
+}
+
+?>
